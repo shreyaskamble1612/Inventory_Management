6732649b85e95bec9286bf5535fd63fe3cb3d57d
shreyaskamble1612 <shreyaskamble393@gmail.com>
2024-03-15 14:42:51 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,19 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 
 const app = express();
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json());
+//Routes
+app.get("/",(req,res)=>{
+    res.send("Home Page");
+})
 
 const PORT = process.env.PORT || 5000;
 
 //Connect to mongo-db and start seerver
-
+ 
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
