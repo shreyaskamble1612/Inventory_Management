9ba625956287fd1c7b419fdb6498841584670d01
shreyaskamble1612 <shreyaskamble393@gmail.com>
2024-03-17 22:55:36 +0530

    new

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,9 @@
 const registerUser = (req,res) => {
     if(!req.body.email){
-        res.status(400)
-        throw new Error("Please add an email");
+        res.status(400).send({error:"Please add an email"})
+    
     }
     res.send("Register User ")
-};
-
-const registerUser = async(req,res) => {
-    try{
-
-    }catch(error){
-
-    }
 };
 module.exports = {
     registerUser,
