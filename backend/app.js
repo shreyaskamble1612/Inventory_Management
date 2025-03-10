72d50cce82d1aa3ff04aeb13a5d723bcfaa76686
shreyaskamble1612 <shreyaskamble393@gmail.com>
2025-03-10 10:30:59 +0530

    CORS

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
     origin:["http://localhost:5173/","https://inventory-management-x54z.onrender.com"],
-    methods:["GET","POST","PUT","DELETE"],
+    methods:["GET","POST","PUT","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"],
     credentials:true,
 }))
