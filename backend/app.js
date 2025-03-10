5a6ea584cef45b881d91cfba3dd922b0fccc2a89
shreyaskamble1612 <shreyaskamble393@gmail.com>
2025-03-10 11:05:48 +0530

    CORS

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,48 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectToMongo = require("./db");
 
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://inventory-management-x54z.onrender.com"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}))
-app.use(bodyParser.json())
-app.options("*",cors()) 
-app.use("/api/user",require("./Routes/user"))
-app.use("/api/item",require("./Routes/item"))
-app.use("/api/log",require("./Routes/log"))
-app.use("/api/auth",require("./Routes/auth"))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-management-x54z.onrender.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(bodyParser.json());
+app.options("*", cors());
+app.use("/api/user", require("./Routes/user"));
+app.use("/api/item", require("./Routes/item"));
+app.use("/api/log", require("./Routes/log"));
+app.use("/api/auth", require("./Routes/auth"));
 app.get("/", (req, res) => {
-    res.send("Hello Shreyas")
-})
+  res.send("Hello Shreyas");
+});
 
 const start = async () => {
-    try {
-        await connectToMongo(MONGO_URI);
-        app.listen(PORT, () => {
-            console.log("Server is running on port " + PORT)
-        })
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    await connectToMongo(MONGO_URI);
+    app.listen(PORT, () => {
+      console.log("Server is running on port " + PORT);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-start()
+start();
